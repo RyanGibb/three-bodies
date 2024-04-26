286058e1600acea694e723f5cd38e95b68645d3c
Emile Trotignon <emile@tarides.com>
2024-04-26 18:49:09 +0200

    commit

--- src/gamelle_example.ml
+++ src/gamelle_example.ml
@@ -8,20 +8,20 @@ let speed_range = Box.v (Point.v (-50.) (-50.)) (Size.v 100. 100.)
 let num_part = 3
 let () = Random.self_init ()
 let g = 1000.
-let collision_damping = 0.9
+let collision_damping = 1.
 let circle_of_particle p = Circle.v p.pos (p.mass /. 100.)
 
 let init =
   List.init num_part (fun _i ->
-  {
-    pos = Box.random_mem box;
-    speed = Box.random_mem speed_range;
-    mass = Random.float 5000. +. 2000.;
-  })
-  (* [ *)
-  (*   { pos = Point.v 300. 600.; speed = Vec.v 100. 0.; mass = 2000. }; *)
-  (*   { pos = Point.v 700. 500.; speed = Vec.v 0. 0.; mass = 10000. }; *)
-  (* ] *)
+      {
+        pos = Box.random_mem box;
+        speed = Box.random_mem speed_range;
+        mass = Random.float 5000. +. 2000.;
+      })
+(* [ *)
+(*   { pos = Point.v 300. 600.; speed = Vec.v 100. 0.; mass = 2000. }; *)
+(*   { pos = Point.v 700. 500.; speed = Vec.v 0. 0.; mass = 10000. }; *)
+(* ] *)
 (* @ [ { *)
 (*     pos = Point.v 500. 500.; *)
 (*     speed = Vec.v 0. 0.; *)
@@ -45,18 +45,21 @@ let collision p1 p2 =
     let { pos = x1; speed = v1; mass = m1 } = p1
     and { pos = x2; speed = v2; mass = m2 } = p2 in
     let open Vec in
-    let speed = collision_damping * (v1
-      - 2. *. m2 /. (m1 +. m2)
-        *. (dot (v1 - v2) (x1 - x2) /. norm2 (x1 - x2))
-        * (x1 - x2)) in
+    let speed =
+      collision_damping
+      * (v1
+        - 2. *. m2 /. (m1 +. m2)
+          *. (dot (v1 - v2) (x1 - x2) /. norm2 (x1 - x2))
+          * (x1 - x2))
+    in
     (* TODO fix collapsed particles flying away when switching window *)
-    let pos = Vec.((Circle.radius c1 +. Circle.radius c2) * (unit (x1 - x2)) + x2) in
+    let pos =
+      Vec.(((Circle.radius c1 +. Circle.radius c2) * unit (x1 - x2)) + x2)
+    in
     Some { pos; speed; mass = m1 }
 
 let collisions p particles =
-  match List.find_map (collision p) particles with
-  | Some p -> p
-  | None -> p
+  match List.find_map (collision p) particles with Some p -> p | None -> p
 
 let update_particle particles p =
   let { pos; speed; mass } = collisions p particles in
@@ -77,6 +80,7 @@ let centre_of_mass particles =
     / total_mass)
 
 let get_drawing_box particles =
+  let padding = 50. in
   let mid = centre_of_mass particles in
   let dx, dy =
     List.fold_left
@@ -86,18 +90,30 @@ let get_drawing_box particles =
           Float.max dy (Float.abs (Vec.y dp)) ))
       (0., 0.) particles
   in
-  (* let dx = Float.max (dx +. 100.) 500. and dy = Float.max (dy +. 100.) 500. in *)
+  let biggest_radius =
+    List.fold_left
+      (fun candidate p ->
+        let radius = Circle.radius (circle_of_particle p) in
+        Float.max radius candidate)
+      0. particles
+  in
+
+  let padding = biggest_radius +. padding in
+  let dx = Float.max (dx +. padding) 100.
+  and dy = Float.max (dy +. padding) 100. in
   Box.v_mid mid (Size.v (dx *. 2.) (dy *. 2.))
 
 let render ~io state =
-  let io = View.drawing_box (get_drawing_box state) io in
+  let io = View.drawing_box ~scale:true ~set_window_size:false (get_drawing_box state) io in
   Box.draw ~io ~color:Color.white (get_drawing_box state);
   Circle.fill ~io ~color:Color.red (Circle.v Vec.zero 1.);
   List.iter (render_particle ~io) state
 
 let () =
   Gamelle.run init @@ fun ~io state ->
+  Window.set_size ~io (Size.v 800. 800.);
   if Event.is_pressed ~io `escape then raise Exit;
+  let state = if Event.is_pressed ~io (`input_char "r") then init else state in
   let state = update state in
   render ~io state;
   state
